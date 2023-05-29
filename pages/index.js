58c0c9da1d547adfb0f5211197bf9552193bf0e4
ShahzodTuraev <turayevshahzodbek@gmail.com>
2023-05-29 17:50:10 +0900

    favicon icon changed

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,7 @@ const index = () => {
     <>
     <Head>
       <title>SHT's Portfolio</title>
+      <link rel="icon" href="/favicon.ico" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
     </Head>
     <Navbar/>
